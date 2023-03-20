0474cb4fc4bc871289deed539373f12e4620a078
Sergio <sergio14camino@gmail.com>
2023-03-20 13:01:45 -0500

    Cambiando las clases del header

--- menu.php
+++ menu.php
@@ -1,24 +1,69 @@
-<ul class="navbar-nav">
-	<li class="nav-item clinicio">
-		<a class="nav-link " href="index.php">INICIO<span class="sr-only">(current)</span></a>
-	</li>
-	<!--    <li class="nav-item  clnosotros">
-		<a class="nav-link " href="index.php#nosotros">¿Qué hacemos?</a>
-	</li> -->
-	<li class="nav-item">
-		<a class="nav-link" href="nosotros.php">NOSOTROS</a>
-	</li>
+<!-- 	<div id="page" class="hfeed site">
+<div class="espaciotop filter-drop-spacer"></div> -->
+<header id="masthead" class="site-header" role="banner">
+	<div class="navmenu">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+			<div class="container">
+				<div class="logo_cont">			
+					<a class="linkLogo" href="index.php">
+						<img class="logo animated fadeInRight" src="img/logo.png" title="Transporte Bella Luz" rel="home" alt="Transporte Bella Luz">
+					</a>
+					<button 
+						class="navbar-toggler" 
+						type="button" 
+						data-toggle="collapse" 
+						data-target="#navbarNav" 
+						aria-controls="navbarNav" 
+						aria-expanded="false" 
+						aria-label="Toggle navigation">							
+						<span class="navbar-toggler-icon"></span>
+					</button>  
+				</div>					
+				<!-- derecha -->
+				<div class="menu_cont">
+					<div class="social-buttons cl-effect-4 ">
+						<div class="menu-menu-principal-container">
+							<a target="_blank" href="https://www.facebook.com/Transportesbellaluz" class="social-button facebook"><i class="fa fa-facebook"></i></a>
+							<a target="_blank" href="https://twitter.com/" class="social-button twitter"><i class="fa fa-twitter"></i></a> 
+							<a target="_blank" href="https://www.youtube.com/" class="social-button dribbble"><i class="fa fa-youtube"></i></a> 
+							<a target="_blank" href="https://www.linkedin.com/" class="social-button linkedin"><i class="fa fa-linkedin"></i></a>
+						</div>
+					<div class="centralTelefonica" style="padding: 10px 0 3px 0;">	
+						<p style="color: #4b565c;"><b>Central Telefónica:</b> <img src="img/icon/telefono-menu.gif">  920 610 632</p>
+					</div> 			
+				</div>
+					<div class="infoHead">								
+						<div 
+							class="collapse navbar-collapse" 
+							id="navbarNav">
+							<ul class="navbar-nav">
+								<li class="nav-item clinicio">
+									<a class="nav-link " href="index.php">INICIO<span class="sr-only">(current)</span></a>
+								</li>
+								<!--    <li class="nav-item  clnosotros">
+									<a class="nav-link " href="index.php#nosotros">¿Qué hacemos?</a>
+								</li> -->
+								<li class="nav-item">
+									<a class="nav-link" href="nosotros.php">NOSOTROS</a>
+								</li>
 
-	<li class="nav-item">
-		<a class="nav-link" href="servicios.php">SERVICIOS</a>
-	</li>
-	<!--       
-	<li class="nav-item ">
-		<a class="nav-link contactoquitaespacioderecho" href="productos.php">PRODUCTOS</a>
-	</li> -->  		      
-	<li class="nav-item ">
-		<a class="nav-link contactoquitaespacioderecho" href="contacto.php">CONTÁCTO</a>
-	</li>        
-</ul>
-
+								<li class="nav-item">
+									<a class="nav-link" href="servicios.php">SERVICIOS</a>
+								</li>
+								<!--       
+								<li class="nav-item ">
+									<a class="nav-link contactoquitaespacioderecho" href="productos.php">PRODUCTOS</a>
+								</li> -->  		      
+								<li class="nav-item ">
+									<a class="nav-link contactoquitaespacioderecho" href="contacto.php">CONTÁCTO</a>
+								</li>        
+							</ul>
+						</div>		        	
+					</div>
+				</div>
+			<!-- derecha -->
+			</div>
+		</nav>
+	</div>
+</header>
 
